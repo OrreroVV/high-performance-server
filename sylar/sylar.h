0934d24d50f18ed3aae9f936c0943dc467fe8769
OrreroVV <1271966049@qq.com>
2024-02-01 14:35:45 +0800

    添加线程

---
+++ sylar/sylar.h
@@ -0,0 +1,10 @@
+#ifndef __SYLAR_SYLAR_H__
+#define __SYLAR_SYLAR_H__
+
+#include "config.h"
+#include "log.h"
+#include "singleton.h"
+#include "util.h"
+#include "thread.h"
+
+#endif
---
+++ sylar/thread.h
@@ -0,0 +1,39 @@
+#ifndef __SYLAR_THREAD_H__
+#define __SYLAR_THREAD_H__
+
+
+#include <thread>
+#include <functional>
+#include <memory>
+#include <pthread.h>
+
+namespace sylar{
+
+class Thread{
+public:
+    typedef std::shared_ptr<Thread> ptr;
+    Thread(std::function<void()> cb, const std::string& name);
+    ~Thread();
+
+    pid_t getId() const;
+    const std::string& getName() const;
+    
+    void join();
+    static Thread* GetThis();
+    static const std::string& GetName();
+    static void SetName(const std::string& name);
+private:
+    Thread(const Thread&) = delete;
+    Thread(const Thread&&) = delete;
+    Thread& operator=(const Thread&) = delete;
+
+    static void* run(void* arg);
+private:
+    pid_t m_id = -1;
+    pthread_t m_thread = 0;
+    std::function<void()> m_cb;
+    std::string m_name;
+};
+};
+
+#endif
