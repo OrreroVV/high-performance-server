8666f7854e937563acaf716de1a2144516d63115
OrreroVV <1271966049@qq.com>
2025-01-03 14:46:46 +0800

    修复Socket在UDP下面的初始化,添加UDP测试

---
+++ copy.sh
@@ -0,0 +1,5 @@
+scp -r ../sylar/sylar .
+scp -r ../sylar/samples .
+scp -r ../sylar/tests .
+scp -r ../sylar/CMakeLists.txt .
+scp -r ../sylar/cmake .
--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -1,96 +1,24 @@
-/*
- * @Author: Orrero
- * @Date: 2024-12-17
- * @Description: 
- * 
- * Copyright (c) 2024 by Orrero, All Rights Reserved. 
- */
 #include "sylar/sylar.h"
-#include <iostream>
-#include <thread>
-#include <functional>
-#include <atomic>
-#include <chrono>
+
 static sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
+void test_fiber() {
+    static int s_count = 5;
+    SYLAR_LOG_INFO(g_logger) << "test in fiber s_count=" << s_count;
 
-
-
-class Timer {
-public:
-    Timer() : running(false) {}
-
-    // 启动定时器（单次执行）
-    void startOnce(int delay_ms, std::function<void()> task) {
-        stop();  // 停止已有的定时器
-        running = true;
-
-        timer_thread = std::thread([this, delay_ms, task]() {
-            std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
-            if (running) {
-                task();
-            }
-        });
+    sleep(1);
+    if(--s_count >= 0) {
+        sylar::Scheduler::GetThis()->schedule(&test_fiber, sylar::GetThreadId());
     }
-
-    // 启动定时器（周期性执行）
-    void startPeriodic(int interval_ms, std::function<void()> task) {
-        stop();  // 停止已有的定时器
-        running = true;
-
-        timer_thread = std::thread([this, interval_ms, task]() {
-            while (running) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
-                if (running) {
-                    task();
-                }
-            }
-        });
-    }
-
-    // 停止定时器
-    void stop() {
-        running = false;
-        if (timer_thread.joinable()) {
-            timer_thread.join();
-        }
-    }
-
-    ~Timer() {
-        stop();
-    }
-
-private:
-    std::thread timer_thread; // 定时器线程
-    std::atomic<bool> running; // 标志定时器是否在运行
-};
-
-
-
-void run_in_fiber() {
-    SYLAR_LOG_INFO(g_logger) << "run_in_fiber begin";
-    Timer timer;
-    sylar::Fiber::ptr fiber = sylar::Fiber::GetThis();
-    SYLAR_LOG_INFO(g_logger) << &*fiber;
-    timer.startOnce(2000, [fiber]() {
-        SYLAR_LOG_INFO(g_logger) << "Single-shot timer executed after 2 seconds!";
-        SYLAR_LOG_INFO(g_logger) << &*fiber;
-        sylar::Scheduler::GetThis()->schedule(fiber);
-    });
-    sylar::Fiber::YieldToHold();
-    
-    SYLAR_LOG_INFO(g_logger) << "run_in_fiber end";
 }
 
 int main(int argc, char** argv) {
     SYLAR_LOG_INFO(g_logger) << "main";
     sylar::Scheduler sc(3, false, "test");
     sc.start();
-    sylar::Fiber::ptr fiber(new sylar::Fiber(&run_in_fiber));
-    sc.schedule(fiber);
-    while (true) {
-        sleep(10);
-    }
+    sleep(2);
+    SYLAR_LOG_INFO(g_logger) << "schedule";
+    sc.schedule(&test_fiber);
     sc.stop();
     SYLAR_LOG_INFO(g_logger) << "over";
     return 0;
